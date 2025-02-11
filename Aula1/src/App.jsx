375f994914c2199906da1340635b12ecbca5a50e
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-02-11 19:36:15 -0300

    projeto iniciado

---
+++ Aula1/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Headers";
+import Main from "./components/Main";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ Aula1/src/components/Main.jsx
@@ -0,0 +1,21 @@
+
+
+const Main = () => {
+  return (
+    <div className="main">
+        <div className="main__texts">
+            <h2>Artistas populares</h2>
+
+            <a href="/"  className="main__link">
+                Mostrar mais 
+            </a>
+
+        </div>
+
+    </div>
+
+
+  );
+};
+
+export default Main;
