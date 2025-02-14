81f91773d683b44214c41497cd5a7db474c2e5e1
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-02-14 18:58:12 -0300

    Ajustes para o deploy 2

--- backend/api/connect.js
+++ backend/api/connect.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import 'dotenv/config';
  
 
-const URI=process.env.STRING_CONEXAO;
+const URI="mongodb+srv://admin:admin123@cluster1.rw4pc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
 
 const client= new MongoClient(URI);
 
