cc239b8b05cdea47319ba52dad7936f16ad12435
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-02-14 19:04:21 -0300

    ajuste no process env

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,10 +1,10 @@
-/* eslint-disable no-undef */
 import 'dotenv/config';
 import axios from 'axios';
 
-const {NODE_ENV}=process.env;
+// const {NODE_ENV}=process.env;
 
-const URL= NODE_ENV === 'development' ? "http://localhost:3001/api" : "/api";
+// const URL= "http://localhost:3001/api";
+const URL= "https://spotify-o6op.onrender.com/api";
 
 const responseArtists= await axios.get(`${URL}/artists`);
 const responseSongs= await axios.get(`${URL}/songs`);
